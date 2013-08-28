684712ea09fbb7caf90f0784748c53a7ae53982b
sterlingm <sterlingm@laptop.(none)>
2013-08-28 12:21:37 -0400

    evaluation_request_handler files added

---
+++ ramp_planner/src/evaluation_request_handler.cpp
@@ -0,0 +1,14 @@
+#include "evaluation_request_handler.h"
+
+
+EvaluationRequestHandler::EvaluationRequestHandler(const ros::NodeHandle& h) : handle_(h) {
+  client_ = handle_.serviceClient<ramp_msgs::EvaluationRequest>("evaluation");
+}
+
+
+const bool EvaluationRequestHandler::request(ramp_msgs::EvaluationRequest& er) {
+  if(client_.call(er)) 
+    return true;
+
+  return false;
+}
