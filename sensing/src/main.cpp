25ecd1fa30894d1fd392d2bfde28cb78947e52ab
sterlingm <sterlingm@laptop.(none)>
2013-10-04 16:55:25 -0400

    Added Subscriber to sensing node and parameter to a launch file The "update_topic_name" parameter has been added to the system_full.launch file. It can be used to specify the topic each sensing node should subscribe to in order to get the other robot's position. The "robot_name" arguments have been added to the system_full.launch file to specify the robots' names.

--- sensing/src/main.cpp
+++ sensing/src/main.cpp
@@ -4,6 +4,7 @@
 #include "ramp_msgs/ObjectList.h"
 #include "ir_object.h"
 #include "tf/transform_datatypes.h"
+#include "ramp_msgs/Update.h"
 
 ramp_msgs::ObjectList list;
 ramp_msgs::Configuration current;
@@ -11,19 +12,16 @@ ros::Publisher pub_obj;
 
 
 /** Get the robot's current configuration so we know where the IR objects are at */
-void updateCallback(const nav_msgs::Odometry& msg) {
+/*void currentCallback(const nav_msgs::Odometry& msg) {
   current.K.clear();
 
   current.K.push_back(msg.pose.pose.position.x);
   current.K.push_back(msg.pose.pose.position.y);
   current.K.push_back(tf::getYaw(msg.pose.pose.orientation));
- /* for(unsigned int i=0;i<msg.configuration.K.size();i++) {
-    current.K.push_back(msg.configuration.K.at(i));
-  }*/
-}
+}*/
 
 /** Get the IR sensor msgs */
-void irCallback(const corobot_msgs::SensorMsg& msg) {
+/*void irCallback(const corobot_msgs::SensorMsg& msg) {
 
   //If infrared
   if(msg.type == msg.INFRARED_FRONT) {
@@ -34,9 +32,15 @@ void irCallback(const corobot_msgs::SensorMsg& msg) {
       list.ir_object = obj.buildIRObjectMsg();
     }
   }
+}*/
+
+
+void updateCallback(const ramp_msgs::Update& u) {
+  
 }
 
 
+/** Publish the list of objects */
 void sendList(const ros::TimerEvent& e) {
   pub_obj.publish(list);
 }
@@ -45,16 +49,27 @@ void sendList(const ros::TimerEvent& e) {
 int main(int argc, char** argv) {
   ros::init(argc, argv, "sensing");
   ros::NodeHandle handle;
+  
 
+  //Get parameters
+  std::string object_topic_name;
+  handle.getParam("sensing/update_topic", object_topic_name);
+  std::cout<<"\nobject_topic_name:"<<object_topic_name;
   
-  ros::Subscriber sub_ir = handle.subscribe("infrared_data", 100, irCallback);
-  ros::Subscriber sub_current = handle.subscribe("odometry", 100, updateCallback);
   
+  //Subscribers
+  ros::Subscriber sub_other_robot = handle.subscribe(object_topic_name, 100, updateCallback);
+  //ros::Subscriber sub_current = handle.subscribe("odometry", 100, currentCallback);
+  //ros::Subscriber sub_ir = handle.subscribe("infrared_data", 100, irCallback);
+  
+
+  //Publishers
   pub_obj = handle.advertise<ramp_msgs::ObjectList>("object_list", 1000);
 
+
+  //Timers
   ros::Timer timer = handle.createTimer(ros::Duration(0.01), sendList);
    
-
 
   std::cout<<"\nSpinning\n";
   ros::spin();
