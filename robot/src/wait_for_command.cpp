71d86b674cf955e1e780934b2c04f794a5fdf553
Sterling McLeod <sterlingmcleod9@gmail.com>
2014-03-04 09:10:32 -0500

    Removed merge characters in wait_for_command.cpp

--- robot/src/wait_for_command.cpp
+++ robot/src/wait_for_command.cpp
@@ -41,15 +41,12 @@ int main(int argc, char** argv) {
   ros::Subscriber sub_traj = handle.subscribe("trajectory", 1000, trajCallback);
   robot.pub_twist_ = handle.advertise<geometry_msgs::Twist>(Corobot::TOPIC_STR_TWIST, 1000);
   
-<<<<<<< HEAD
   robot.pub_twist_ = handle.advertise<geometry_msgs::Twist>(Corobot::TOPIC_STR_TWIST, 1000);
   
   std::cout<<"\nSpinning\n";
   ros::spin();
 
-=======
   ros::spin();
->>>>>>> b5e22d877ad3932610e8ad8436a9b7e2ce625e55
   std::cout<<"\nExiting Normally\n";
   return 0;
 }
