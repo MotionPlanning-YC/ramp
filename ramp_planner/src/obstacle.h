4fb23219f9f3a47843741910fe081d3c0ee8d8ff
Sterling McLeod <sterlingmcleod9@gmail.com>
2015-06-17 17:10:47 -0400

    Added Obstacle .h and .cpp

---
+++ ramp_planner/src/obstacle.h
@@ -0,0 +1,16 @@
+#ifndef OBSTACLE_H
+#define OBSTACLE_H
+#include "utility.h"
+#include "path.h"
+
+class Obstacle
+{
+  public:
+    Obstacle() {}
+    ~Obstacle() {}
+
+
+    Path p_;
+  private:
+};
+#endif
