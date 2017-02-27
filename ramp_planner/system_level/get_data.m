93eb63ef2169bb4866e53bddebdd7f1bcf6649d9
Sterling McLeod <sterlingmcleod9@gmail.com>
2017-02-27 11:23:19 -0500

    Re-arranged the order of the directories so that they go in increasing order Removing first set of 100 data points from ABTCs 0,0,0 and 0,2,4 so that it's easier to use them in Matlab. Added a copy of the original data to the repo too.

--- ramp_planner/system_level/get_data.m
+++ ramp_planner/system_level/get_data.m
@@ -1,8 +1,32 @@
-f_fe = 'feasible.txt';
-f_re = 'reached_goal.txt';
-f_ic_st = 'ic_stuck.txt';
-f_ic_oc = 'ic_occurred.txt';
-f_tl = 'time_left.txt';
+directory_prefix = 'advanced_robotics';
+abtc = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+% Initialize the cell arrays to hold the filenames
+f_fe = cell(size(abtc));
+f_re = cell(size(abtc));
+f_ic_st = cell(size(abtc));
+f_ic_oc = cell(size(abtc));
+f_tl = cell(size(abtc));
+
+% Set the filenames
+for i=1:size(abtc,2)
+    f_prefix = fullfile(directory_prefix, abtc(i));
+    
+    f_fe(i) = {fullfile(f_prefix, 'feasible.txt')};
+    f_re(i) = {fullfile(f_prefix, 'reached_goal.txt')};
+    f_ic_st = {fullfile(f_prefix, 'ic_stuck.txt')};
+    f_ic_oc = {fullfile(f_prefix, 'ic_occurred.txt')};
+    f_tl = {fullfile(f_prefix, 'time_left.txt')};
+end
+
+
+data_fe = importdata(f_fe{1});
+    
+% Go through each f_fe
+for i=1:size(f_fe,2)
+    data_fe(i) = importdata(f_fe{i});
+end
+
 
 fe = importdata(strcat('1/', f_fe));
 fe = [fe; importdata(strcat('2/', f_fe))];
