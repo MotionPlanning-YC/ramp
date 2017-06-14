3150e5d2de77b63e4b2c735478f6b3819966e34a
Sterling McLeod <sterlingmcleod9@gmail.com>
2017-06-14 15:48:50 -0400

    Added support for multiple obstacles in move_gazebo_obs node. The model names are specified in config.yaml

--- ramp_planner/src/main_gazebo_ob.cpp
+++ ramp_planner/src/main_gazebo_ob.cpp
@@ -6,10 +6,11 @@
 #include "utility.h"
 
 Utility u;
-ramp_msgs::RampTrajectory ob_trj;
+std::vector<ramp_msgs::RampTrajectory> ob_trjs;
 ros::Time t_start;
 ros::ServiceClient gz_set_model;
 bool first=true;
+std::vector<std::string> model_names;
 
 /*
  * Publish obstacle information at 20Hz to simulate sensing cycles
@@ -19,23 +20,25 @@ void pubObTrj(const ros::TimerEvent e)
   //ROS_INFO("In pubObTrj");
 
   ros::Duration d_elapsed = ros::Time::now() - t_start;
-  
   int index = d_elapsed.toSec()*10;
   ROS_INFO("t_start: %f ros::Time::now(): %f d_elapsed: %f index: %i", t_start.toSec(), ros::Time::now().toSec(), d_elapsed.toSec(), index);
-  
-  // Get the point
-  trajectory_msgs::JointTrajectoryPoint p = index >= ob_trj.trajectory.points.size() ? ob_trj.trajectory.points[ob_trj.trajectory.points.size()-1] : ob_trj.trajectory.points[index]; 
 
-  // Build srv to send
-  gazebo_msgs::SetModelState ms; 
-  ms.request.model_state.model_name = "cardboard_box";
-  ms.request.model_state.pose.position.x = p.positions[0];
-  ms.request.model_state.pose.position.y = p.positions[1];
-
-  // Call srv
-  if(!gz_set_model.call(ms))
+  for(int i=0;i<ob_trjs.size();i++)
   {
-    ROS_ERROR("Error calling set_model_state");
+    // Get the point
+    trajectory_msgs::JointTrajectoryPoint p = index >= ob_trjs[i].trajectory.points.size() ? ob_trjs[i].trajectory.points[ob_trjs[i].trajectory.points.size()-1] : ob_trjs[i].trajectory.points[index]; 
+
+    // Build srv to send
+    gazebo_msgs::SetModelState ms; 
+    ms.request.model_state.model_name = i == 0 ? "cardboard_box" : "cardboard_box_0";
+    ms.request.model_state.pose.position.x = p.positions[0];
+    ms.request.model_state.pose.position.y = p.positions[1];
+
+    // Call srv
+    if(!gz_set_model.call(ms))
+    {
+      ROS_ERROR("Error calling set_model_state");
+    }
   }
 }
 
@@ -50,75 +53,84 @@ int main(int argc, char** argv)
   ros::ServiceClient gz_get_model = handle.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
   gz_set_model = handle.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
 
-  // Build a trajectory request
-  ramp_msgs::TrajectoryRequest tr;
-
-  // Build a path
-  ramp_msgs::Path p;
-
-  // Get initial state of the object
-  std::string model_name = "cardboard_box";
-  gazebo_msgs::GetModelState cb;
-  cb.request.model_name = model_name;
-
-  if(gz_get_model.call(cb))
+  handle.getParam("/ramp/gazebo_model_names", model_names);
+  for(int i=0;i<model_names.size();i++)
   {
-    ROS_INFO("%s pose: position (%f,%f,%f) orientation [%f,%f,%f,%f]", model_name.c_str(), cb.response.pose.position.x, cb.response.pose.position.y, cb.response.pose.position.z, cb.response.pose.orientation.x, cb.response.pose.orientation.y, cb.response.pose.orientation.z, cb.response.pose.orientation.w);
-  }
-  else
-  {
-    ROS_ERROR("Error calling get_model_state");
+    ROS_INFO("model_names[%i]: %s", i, model_names[i].c_str());
   }
 
-
-
-  /*
-   * 
-   */
-
-  // Build the knotpoints of the path
-  ramp_msgs::KnotPoint kp1;
-  kp1.motionState.positions.push_back(cb.response.pose.position.x);
-  kp1.motionState.positions.push_back(cb.response.pose.position.y);
-
-  // Make sure deltaX > deltaY
-  ramp_msgs::KnotPoint kp2;
-  kp2.motionState.positions.push_back(cb.response.pose.position.x-8);
-  kp2.motionState.positions.push_back(cb.response.pose.position.y);
-  kp2.motionState.positions.push_back(PI);
-
-  double theta1 = u.findAngleFromAToB(kp1.motionState.positions, kp2.motionState.positions);
-  kp1.motionState.positions.push_back(PI);
-
-
-  ramp_msgs::KnotPoint kp3;
-  kp3.motionState.positions.push_back(cb.response.pose.position.x-2.0);
-  kp3.motionState.positions.push_back(cb.response.pose.position.y);
-  kp3.motionState.positions.push_back(PI);
-
-  // Push knotpoints onto the path
-  p.points.push_back(kp1);
-  p.points.push_back(kp2);
-  //p.points.push_back(kp3);
-  
-
-  // Build the request
-  tr.path = p;
-  tr.type = HOLONOMIC;
-
-  // Build the srv
-  ramp_msgs::TrajectorySrv ts;
-  ts.request.reqs.push_back(tr);
-
-  // Call trajectory_generator
-  if(client.call(ts))
+  for(int i=0;i<2;i++)
   {
-    ROS_INFO("Trajectory: %s", u.toString(ts.response.resps[0].trajectory).c_str());
-    ob_trj = ts.response.resps[0].trajectory;
-  }
-  else
-  {
-    ROS_ERROR("Error calling trajectory_generator");
+    // Get initial state of the object
+    std::string model_name = model_names[i];
+    gazebo_msgs::GetModelState cb;
+    cb.request.model_name = model_name;
+
+    if(gz_get_model.call(cb))
+    {
+      ROS_INFO("%s pose: position (%f,%f,%f) orientation [%f,%f,%f,%f]", model_name.c_str(), cb.response.pose.position.x, cb.response.pose.position.y, cb.response.pose.position.z, cb.response.pose.orientation.x, cb.response.pose.orientation.y, cb.response.pose.orientation.z, cb.response.pose.orientation.w);
+    }
+    else
+    {
+      ROS_ERROR("Error calling get_model_state");
+    }
+
+    // Build a trajectory request
+    ramp_msgs::TrajectoryRequest tr;
+
+    // Build a path
+    ramp_msgs::Path p;
+
+
+    /*
+     * 
+     */
+
+    // Build the knotpoints of the path
+    ramp_msgs::KnotPoint kp1;
+    kp1.motionState.positions.push_back(cb.response.pose.position.x);
+    kp1.motionState.positions.push_back(cb.response.pose.position.y);
+
+    // Make sure deltaX > deltaY
+    ramp_msgs::KnotPoint kp2;
+    kp2.motionState.positions.push_back(cb.response.pose.position.x-8);
+    kp2.motionState.positions.push_back(cb.response.pose.position.y);
+    kp2.motionState.positions.push_back(PI);
+
+    double theta1 = u.findAngleFromAToB(kp1.motionState.positions, kp2.motionState.positions);
+    kp1.motionState.positions.push_back(PI);
+
+
+    ramp_msgs::KnotPoint kp3;
+    kp3.motionState.positions.push_back(cb.response.pose.position.x-2.0);
+    kp3.motionState.positions.push_back(cb.response.pose.position.y);
+    kp3.motionState.positions.push_back(PI);
+
+    // Push knotpoints onto the path
+    p.points.push_back(kp1);
+    p.points.push_back(kp2);
+    //p.points.push_back(kp3);
+    
+
+    // Build the request
+    tr.path = p;
+    tr.type = HOLONOMIC;
+
+    // Build the srv
+    ramp_msgs::TrajectorySrv ts;
+    ts.request.reqs.push_back(tr);
+
+    // Call trajectory_generator
+    if(client.call(ts))
+    {
+      ROS_INFO("Trajectory: %s", u.toString(ts.response.resps[0].trajectory).c_str());
+      ob_trjs.push_back(ts.response.resps[0].trajectory);
+    }
+    else
+    {
+      ROS_ERROR("Error calling trajectory_generator");
+    }
+
   }
   
   // Make Timer to start moving
