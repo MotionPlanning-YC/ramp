7c1bcaa54fd84b0c8197938f4c2bc871bc731038
Sterling McLeod <sterlingmcleod9@gmail.com>
2017-06-16 15:07:32 -0400

    Added some bag files and a new node to publish a map frame that is equal to the odom frame

---
+++ ramp_sensing/src/main_map_equal_odom.cpp
@@ -0,0 +1,50 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <turtlesim/Pose.h>
+
+std::string turtle_name;
+
+/*
+ * This node will publish a map frame that is equal to the odom frame
+ * This is useful when we aren't using a map, but need to specify an initial pose
+ */
+
+
+void sendTF(const ros::TimerEvent& e)
+{
+  //ROS_INFO("In sendTF");
+
+  // Create broadcaster
+  static tf::TransformBroadcaster br;
+
+  // Create transform
+  tf::Transform transform;
+
+  // Set position vector
+  transform.setOrigin( tf::Vector3(0, 0, 0) );
+
+  // Set rotation
+  // 0.9 is 51 degrees, it is determined by the map's orientation
+  // we want the costmap to have the same orientation as the map's hallway
+  tf::Quaternion q;
+  q.setRPY(0, 0, 0);
+  transform.setRotation(q);
+
+  //ROS_INFO("Time::now(): %f", ros::Time::now().toSec());
+
+  // Broadcast to tf
+  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "odom"));
+}
+
+int main(int argc, char** argv) 
+{
+  ros::init(argc, argv, "my_tf_broadcaster");
+  ros::NodeHandle handle;
+
+  ros::Timer tf_timer = handle.createTimer(ros::Duration(0.1), &sendTF);
+
+  ROS_INFO("Timer created");
+
+  ros::spin();
+  return 0;
+}
